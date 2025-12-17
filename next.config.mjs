46c57027cfd863d25dd3c7c6919f0b18d8f5d063
Nishant Bk <125780674+nishantbk311@users.noreply.github.com>
2025-12-17 22:28:12 +0545

    Updated code

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    qualities: [25, 50, 75, 80],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "gbyzopuysrtcsoougtzr.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
