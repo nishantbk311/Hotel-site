3099aee8b6bd125ba01bdf5b9cfccfc5b7803669
Nishant Bk <125780674+nishantbk311@users.noreply.github.com>
2025-12-30 00:54:34 +0545

    Updated code

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,4 +1,4 @@
-import { getCabin } from "@/app/_lib/data-service";
+import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
 
@@ -23,6 +23,16 @@ export async function generateMetadata({ params }) {
   const { cabinId } = await params;
   const { name } = await getCabin(cabinId);
   return { title: `Cabin ${name}` };
+}
+
+// telling Next.js ahead of time ....
+// we need to return array of objects having cabinId property (page name)
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  const ids = cabins.map((cabin) => ({ cabinId: String(cabin.id) }));
+
+  return ids;
 }
 
 export default async function Page({ params }) {
